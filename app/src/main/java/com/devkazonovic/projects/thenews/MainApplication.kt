c385d7f230554043707e418f8a6d1b77605b8877
DevKazonovic <kazonovic.yasser.69@gmail.com>
2021-12-05 21:24:05 +0100

    SetUp Main Dependencies

---
+++ app/src/main/java/com/devkazonovic/projects/thenews/MainApplication.kt
@@ -0,0 +1,20 @@
+package com.devkazonovic.projects.thenews
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+import timber.log.Timber
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+            Timber.plant(object : Timber.DebugTree(){
+                override fun createStackElementTag(element: StackTraceElement): String {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+    }
+}
